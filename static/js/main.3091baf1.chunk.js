4aea40922759358eb369abf678736c1eea3f856a
unknown <eyunsuh2@yonsei.ac.kr>
2021-01-07 21:41:04 +0900

    Updates

---
+++ static/js/main.3091baf1.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-homepage"]=this["webpackJsonpmy-homepage"]||[]).push([[0],{9:function(e,t,n){"use strict";n.r(t);var c=n(0),o=n(1),i=n.n(o),r=n(3);var s=function(){return Object(c.jsx)("div",{})},a=function(e){e&&e instanceof Function&&n.e(3).then(n.bind(null,10)).then((function(t){var n=t.getCLS,c=t.getFID,o=t.getFCP,i=t.getLCP,r=t.getTTFB;n(e),c(e),o(e),i(e),r(e)}))};n.n(r).a.render(Object(c.jsx)(i.a.StrictMode,{children:Object(c.jsx)(s,{})}),document.getElementById("root")),a()}},[[9,1,2]]]);
+//# sourceMappingURL=main.3091baf1.chunk.js.map
